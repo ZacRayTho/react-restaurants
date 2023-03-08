f23b63b4e10d62e6528c7b403e5c5b0fec4a8203
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-03-08 22:41:17 +0000

    added buttons to menu page for filtering catagories

--- gimme-food/src/pages/Menu.js
+++ gimme-food/src/pages/Menu.js
@@ -6,6 +6,8 @@ import Food from "../components/Food";
 export default function Menu() {
 
     const [food, setFood] = useState([])
+    const [filter, setFilter] = useState("All")
+
     useEffect(
         function apiCall() {
             let options = {
@@ -24,6 +26,10 @@ export default function Menu() {
         }
         , [])
 
+        function handleClick(category) {
+            setFilter(category)
+        }
+
 
     return (
         <>
@@ -31,7 +37,14 @@ export default function Menu() {
 
             <div className="container bg-img2 w-75 d-flex justify-content-center row mx-auto">
                 <h1 className="text-center m-1" style={{ fontSize: "100px", color: "white", fontFamily: "chalk"}}>Menu</h1>
-                
+                <div className="text-center btn-group bg-white w-75">
+                    <button className="btn btn-transparent" onClick={() => {handleClick("All")}}>All</button>
+                    <button className="btn btn-transparent" onClick={() => {handleClick("Breakfast")}}>Breakfast</button>
+                    <button className="btn btn-transparent" onClick={() => {handleClick("Lunch")}}>Lunch</button>
+                    <button className="btn btn-transparent" onClick={() => {handleClick("Appetizer")}}>Appetizer</button>
+                    <button className="btn btn-transparent" onClick={() => {handleClick("Dinner")}}>Dinner</button>
+                    <button className="btn btn-transparent" onClick={() => {handleClick("Drink")}}>Drink</button>
+                </div>
 
                 {food.map((item, index) => <Food item={item} key={index}></Food>)}
 

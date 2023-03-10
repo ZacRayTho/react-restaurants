542ca9fbf3540db263612d1af24f625ed397df20
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-03-10 03:03:28 +0000

    weird bug where remove item in cart removes from local storage but checkout remove doesnt

--- gimme-food/src/pages/Checkout.js
+++ gimme-food/src/pages/Checkout.js
@@ -1,16 +1,16 @@
 import Nav from "../components/Nav"
 import CartItem from "../components/CartItem";
 //bring cart info in
-export default function Checkout(props){
-    const{ cart, setCart } = props;
+export default function Checkout(props) {
+    const { cart, setCart } = props;
 
-    return(
+    return (
         <>
-        <Nav />
-        <div className="bg-white ">
-        <h1>Checkout</h1>
-        {cart.map((item, index) => <CartItem cart={cart} index={index} item={item} setCart={setCart} key={index} />)}
-        </div>
+            <Nav />
+            <div className="bg-white ">
+                <h1>Checkout</h1>
+                {cart.map((item, index) => <CartItem cart={cart} index={index} item={item} setCart={setCart} key={index} />)}
+            </div>
         </>
     )
 }
